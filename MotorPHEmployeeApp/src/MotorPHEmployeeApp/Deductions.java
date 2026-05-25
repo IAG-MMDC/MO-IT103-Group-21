e4f7e8a53aac4412fe1f53b0bfeafa4004112ea4
lrjamrosales-droid <lr.jamrosales@mmdc.mcl.edu.ph>
2026-05-25 18:22:33 +0800

    Update Deductions.java

--- MotorPHEmployeeApp/src/MotorPHEmployeeApp/Deductions.java
+++ MotorPHEmployeeApp/src/MotorPHEmployeeApp/Deductions.java
@@ -6,7 +6,7 @@ package MotorPHEmployeeApp;
 
 /**
  *
- * @author Home
+ * @author Group21 - H1101
  */
 public class Deductions {
     
@@ -32,7 +32,7 @@ public class Deductions {
     static final double PHILHEALTH_MIN_EMP_SHARE = 150.00;
     static final double PHILHEALTH_MAX_EMP_SHARE = 900.00;
     
-    // Withholding Tax - Monthly Rate Constants 
+    // Withholding Tax - Maximum Income Limit Per Tier Constants 
     static final int TAX_BRACKET_1_MAX = 20_832;
     static final int TAX_BRACKET_2_MAX = 33_332;
     static final int TAX_BRACKET_3_MAX = 66_666;
@@ -74,9 +74,11 @@ public class Deductions {
         return contribution;
     }
 
-    /*
-    
-    */
+    /* PagIBIG Monthly Contribution Computation
+     * Salary Below PHP 1000            -> PHP 0 
+     * Salary At Least PHP 1000 to 1500 -> 1%, Capped at PHP 100
+     * Salary Over PHP 1500             -> 2%, Capped at PHP 100
+     */
     public static double computePagIBIGContribution(double grossMonthlySalary) {
         
         if (grossMonthlySalary >= PAGIBIG_MIN_SALARY_THRESH && grossMonthlySalary <= PAGIBIG_MAX_SALARY_THRESH) {
@@ -92,9 +94,12 @@ public class Deductions {
         }
     }
     
-    /*
-    
-    */
+    /* PhilHealth Monthly Contribution Computation (Employee Share)
+     * Premium rate is divided by two to get employee share.
+     * Salary At Or Below PHP 10000           -> PHP 150 
+     * Salary Between PHP 10000 And PHP 60000 -> (Gross Salary * .03) / 2
+     * Salary At Or Above PHP 60000           -> PHP 900 
+     */
     public static double computePhilHealthContribution(double grossMonthlySalary) {
         
         if (grossMonthlySalary <= PHILHEALTH_MIN_MO_BASIC_SALARY) {
@@ -108,7 +113,7 @@ public class Deductions {
         return (grossMonthlySalary * PHILHEALTH_PREM_RATE) / 2;  
     }
 
-    
+    // Total Deductions Computation
     public static double computeTotalDeductions(
             double sss, 
             double philhealth, 
@@ -117,7 +122,7 @@ public class Deductions {
         return sss + philhealth + pagibig;
     }
 
-    
+    // Taxable Income Computation
     public static double computeTaxableIncome(
             double grossMonthlySalary, 
             double totalDeductions
@@ -125,19 +130,25 @@ public class Deductions {
         return grossMonthlySalary - totalDeductions;
     }
     
-    
+    // Withholding tax computation based on government-mandated deductions/contributions matrix.
     public static double computeWithholdingTax(double taxableIncome) { 
-        
+
+        // No withholding tax
         if (taxableIncome <= TAX_BRACKET_1_MAX) return 0;
-        
+
+        // 20% in excess of PHP 20,833
         else if (taxableIncome <= TAX_BRACKET_2_MAX) return (taxableIncome - 20_833) * TAX_RATE_1;
-        
+
+        // PHP 2,500 + 25% in excess of PHP 33,333
         else if (taxableIncome <= TAX_BRACKET_3_MAX) return (taxableIncome - 33_333) * TAX_RATE_2 + BASE_TAX_1;
-        
+
+        // PHP 10,833 + 30% in excess of PHP 66,667
         else if (taxableIncome <= TAX_BRACKET_4_MAX) return (taxableIncome - 66_667) * TAX_RATE_3 + BASE_TAX_2;
-        
+
+        // PHP 40,833.33 + 32% in excess over PHP 166,667
         else if (taxableIncome <= TAX_BRACKET_5_MAX) return (taxableIncome - 166_667) * TAX_RATE_4 + BASE_TAX_3;
-        
+
+        // PHP 200,833.33 + 35% in excess of 666,667
         else return (taxableIncome - 666_667) * TAX_RATE_5 + BASE_TAX_4;
     }
 }
